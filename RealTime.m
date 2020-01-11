65f3dd3604b9eb67a45f9c1028033ab7f253ed06
105319015Hong-Yong-Jie <32547569+105319015Hong-Yong-Jie@users.noreply.github.com>
2020-01-11 16:29:08 +0800

    Add files via upload

---
+++ RealTime.m
@@ -0,0 +1,9 @@
+% function sys = RealTime(fs, showTimes, mode)
+% %%     By Small Potato 
+% %     Real-time audio waves plot
+% %     sys = RealTime(fs, showTimes, mode)
+% %     Optional Input:
+% %       fs         Sampling frequency (Hz). Default is 16k.
+% %       showTimes  Show signal (sec). Default is 3 sec.
+% %       Mode       0 is not output audio files.  1 is output audio files in showTimes. 
+% end
